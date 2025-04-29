b68424d527c5b00161668b232c780993d01298cb
gGorn <106643261+gGorn@users.noreply.github.com>
2025-04-29 20:16:37 +0700

    initial project

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="d-flex">
+    <div class="flex-grow-1">
+      <Navbar />
+      <div class="p-4 container">
+        <RouterView />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Navbar from '../components/Navbar.vue'
+import { RouterView } from 'vue-router'
+</script>
